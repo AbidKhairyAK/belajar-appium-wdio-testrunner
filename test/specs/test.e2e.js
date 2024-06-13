c06374de870acf81bfa0c5e30ce86e0a3d7a4a3c
AbidKhairyAK <abid.khairy.ak@gmail.com>
2024-06-13 17:31:08 +0700

    feat: tambahin cara scroll

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,6 +1,25 @@
 import { $, driver, expect } from '@wdio/globals'
+import scrollScreen from '../../helpers/scrollScreen.js'
 
 describe('coba test android', function () {
+    it ('coba scroll ke atas bawah', async function () {
+        await scrollScreen(600, 100)
+        await scrollScreen(100, 600)
+
+        await driver.pause(2000)
+    })
+
+    it ('tekan tombol locked_out_user untuk mengisi form login', async function () {
+        const usernameInput = await $('~test-Username')
+        const lockUserButton = await $('//*[@content-desc="test-locked_out_user"]')
+
+        await scrollScreen(1000, 100)
+        await lockUserButton.click()
+        await scrollScreen(100, 1000)
+
+        expect(usernameInput).toHaveValue('locked_out_user')
+    })
+
     it('login dengan username yang benar tapi password salah',  async function () {
         const usernameInput = await $('~test-Username')
         const passwordInput = await $('~test-Password')
